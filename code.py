a29dcbc056b84e19912c8860fa6a4a5c6afbd463
shashank98giri <g.shashank700@gmail.com>
2018-05-07 18:40:19 +0530

    changed typing status

--- code.py
+++ code.py
@@ -6,6 +6,7 @@ from fbchat import Client
 from fbchat.models import *
 import datetime
 from getpass import getpass
+import time
 
 now= datetime.datetime.now()
 k=now.strftime("%d-%m")
@@ -15,16 +16,19 @@ username=input("Username:")
 
 
 if k in cont:
-    
+	
+    client = Client(username, getpass())
     patt1=r"([\w]+) ([\w]+):"+k
     mat=re.findall(patt1,cont)
     for _,ele in enumerate(mat):
-        name=ele[0]+" "+ele[1]    
-        client = Client(username, getpass())
+        name=ele[0]+" "+ele[1]  
+        
         users = client.searchForUsers(name)
-        user = users[0]    
-        client.sendLocalImage("cake.jpg", 'Happy Birthday' + name, thread_id=user.uid, thread_type=ThreadType.USER)
-    
+        user = users[0]
+        client.setTypingStatus(TypingStatus.TYPING,user.uid)
+        time.sleep(2)   
+        client.sendLocalImage("cake.jpg", 'Happy Birthday !!!\nEnjoy ^_^ ^_^ ', thread_id=user.uid, thread_type=ThreadType.USER)
+    print("wished {} users".format(len(mat)))
 else:
     print("NO friends have Birthday today.")
 
