173c1c4f118ddb49c754f8f3be805648de65ea73
shashank98giri <g.shashank700@gmail.com>
2018-06-06 15:15:04 +0530

    Removed reduntant lines

--- code.py
+++ code.py
@@ -1,6 +1,4 @@
-#Automatic Birthday wisher on facebook background version i.e
-# not showing window and gui to user.
-#One time set up, Paste it into Startup folder.
+
 import re
 from fbchat import Client
 from fbchat.models import *
@@ -16,23 +14,19 @@ username=input("Username:")
 
 
 if k in cont:
-	
+
     client = Client(username, getpass())
     patt1=r"([\w]+) ([\w]+):"+k
     mat=re.findall(patt1,cont)
     for _,ele in enumerate(mat):
-        name=ele[0]+" "+ele[1]  
-        
+        name=ele[0]+" "+ele[1]
+
         users = client.searchForUsers(name)
         user = users[0]
         client.setTypingStatus(TypingStatus.TYPING,user.uid)
-        time.sleep(2)   
+        time.sleep(2)
         client.sendLocalImage("cake.jpg", 'Happy Birthday !!!\nEnjoy ^_^ ^_^ ', thread_id=user.uid, thread_type=ThreadType.USER)
     print("wished {} users".format(len(mat)))
+
 else:
     print("NO friends have Birthday today.")
-
-
-
-
-
